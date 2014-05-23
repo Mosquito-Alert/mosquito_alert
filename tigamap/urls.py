6e1957e7851aa3649ef9b9043b7f5e69dbb4c66d
John Palmer <johnrbpalmer@gmail.com>
2014-05-23 12:54:13 +0200

    New webmap urls

--- tigamap/urls.py
+++ tigamap/urls.py
@@ -3,5 +3,12 @@ from tigamap import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.show_fixes, name='show_fixes')
+    url(r'^$', views.show_fixes, name='show_fixes'),
+    url(r'^embedded/$', views.show_fixes, name='show_fixes'),
+    url(r'^embedded/ca/$', views.show_fixes, name='show_fixes'),
+    url(r'^embedded/es/$', views.show_fixes, name='show_fixes'),
+    url(r'^embedded/en/$', views.show_fixes, name='show_fixes'),
+    url(r'^ca/$', views.show_fixes, name='show_fixes'),
+    url(r'^es/$', views.show_fixes, name='show_fixes'),
+    url(r'^en/$', views.show_fixes, name='show_fixes'),
 )
