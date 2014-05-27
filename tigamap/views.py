6b685ebde23e3fcab5623859076a80d9a44b7a63
John Palmer <johnrbpalmer@gmail.com>
2014-05-27 15:00:06 +0200

    Made embedded map view exempt from clickjacking protection so it can be used in iFrames

--- tigamap/views.py
+++ tigamap/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tigaserver_app.models import Fix
-
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
@@ -13,6 +13,7 @@ def show_webmap_app(request):
     return render(request, 'tigamap/app.html', context)
 
 
+@xframe_options_exempt
 def show_embedded_webmap(request):
     fix_list = Fix.objects.all()
     context = {'fix_list': fix_list}
