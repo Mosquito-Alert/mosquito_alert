32249825d4eb46e9bea6d5116ea016eeea3b0b78
John Palmer <johnrbpalmer@gmail.com>
2014-05-29 21:04:02 +0200

    Created pages for help, about, license, policies, terms, and privacy

--- tigahelp/views.py
+++ tigahelp/views.py
@@ -3,16 +3,41 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def show_help_ca(request):
+def show_about(request, platform, language):
     context = {}
-    return render(request, 'tigahelp/ca.html', context)
+    if language == 'ca':
+        return render(request, 'tigahelp/about_ca.html', context)
+    if language == 'es':
+        return render(request, 'tigahelp/about_es.html', context)
+    if language == 'en':
+        return render(request, 'tigahelp/about_en.html', context)
 
 
-def show_help_es(request):
+def show_help(request, platform, language):
     context = {}
-    return render(request, 'tigahelp/es.html', context)
+    if language == 'ca':
+        return render(request, 'tigahelp/help_ca.html', context)
+    if language == 'es':
+        return render(request, 'tigahelp/help_es.html', context)
+    if language == 'en':
+        return render(request, 'tigahelp/help_en.html', context)
 
 
-def show_help_en(request):
+def show_license(request, platform, language):
     context = {}
-    return render(request, 'tigahelp/en.html', context)
+    if language == 'ca':
+        return render(request, 'tigahelp/license_ca.html', context)
+    if language == 'es':
+        return render(request, 'tigahelp/license_es.html', context)
+    if language == 'en':
+        return render(request, 'tigahelp/license_en.html', context)
+
+
+def show_policies(request, language):
+    context = {}
+    if language == 'ca':
+        return render(request, 'tigahelp/policies_ca.html', context)
+    if language == 'es':
+        return render(request, 'tigahelp/policies_ca.html', context)
+    if language == 'en':
+        return render(request, 'tigahelp/policies_ca.html', context)
--- tigaserver_project/urls.py
+++ tigaserver_project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import *
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from tigahelp.views import show_help, show_about, show_license, show_policies
 
 admin.autodiscover()
 
@@ -12,5 +13,10 @@ urlpatterns = patterns('',
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('tigaserver_app.urls')),
     url(r'^webmap/', include('tigamap.urls')),
-    url(r'^help/', include('tigahelp.urls')),
+    url(r'^help/(?P<platform>\w+)/(?P<language>\w+)/$', show_help),
+    url(r'^about/(?P<platform>\w+)/(?P<language>\w+)/$', show_about),
+    url(r'^license/(?P<platform>\w+)/(?P<language>\w+)/$', show_license),
+    url(r'^policies/(?P<language>\w+)/$', show_policies),
+    url(r'^terms/(?P<language>\w+)/$', show_policies),
+    url(r'^privacy/(?P<language>\w+)/$', show_policies),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
