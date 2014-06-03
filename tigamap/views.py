a378d960ac8bc8d7acd9360aaaa20cbe2ec3591e
John Palmer <johnrbpalmer@gmail.com>
2014-06-03 15:59:46 +0200

    Added needed markercluster css and changed coverage map to .05

--- tigamap/views.py
+++ tigamap/views.py
@@ -23,34 +23,10 @@ def show_coverage_map(request):
     return render(request, 'tigamap/coverage_map.html', context)
 
 
-def show_coverage_map_25(request):
-    fix_list = Fix.objects.all()
-    context = {'fix_list': fix_list}
-    return render(request, 'tigamap/coverage_map_25.html', context)
-
-
 def show_grid_05(request):
     fix_list = Fix.objects.all()
     context = {'fix_list': fix_list}
     return render(request, 'tigamap/grid_map.05.html', context)
-
-
-def show_grid_1(request):
-    fix_list = Fix.objects.all()
-    context = {'fix_list': fix_list}
-    return render(request, 'tigamap/grid_map.1.html', context)
-
-
-def show_grid_25(request):
-    fix_list = Fix.objects.all()
-    context = {'fix_list': fix_list}
-    return render(request, 'tigamap/grid_map.25.html', context)
-
-
-def show_grid_5(request):
-    fix_list = Fix.objects.all()
-    context = {'fix_list': fix_list}
-    return render(request, 'tigamap/grid_map.5.html', context)
 
 
 def show_report_map(request):
