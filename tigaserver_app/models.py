0b60ca2a899b1c30b0c6938d023968fb205e8820
John Palmer <johnrbpalmer@gmail.com>
2014-06-13 15:33:15 +0200

    minor change in tigaprob code

--- tigaserver_app/models.py
+++ tigaserver_app/models.py
@@ -258,7 +258,7 @@ class Report(models.Model):
             if this_response.answer == 'No':
                 result -= 1
         if total == 0:
-            total = 1;
+            total = 1
         return result/total
 
     def get_tigaprob_text(self):
