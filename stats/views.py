13b29ab58f950745cfe79655f96a60a0a1722ace
John Palmer <johnrbpalmer@gmail.com>
2014-06-16 19:29:00 +0200

    minor adjustments to stats chart

--- stats/views.py
+++ stats/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from tigaserver_app.models import *
-from django.db.models import Count, Min, Max
 from datetime import date, timedelta
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
+@xframe_options_exempt
 def show_usage(request):
-    ref_date = date(2014, 5, 13)
+    ref_date = date(2014, 6, 13)
     end_date = date.today()
-
     days = []
     users = []
-    while ref_date < end_date:
+    while ref_date <= end_date:
         days.append(ref_date.strftime("%x"))
         ref_date += timedelta(days=1)
-        users.append(len(TigaUser.objects.filter(registration_time__lt=ref_date)))
+        users.append(len(TigaUser.objects.filter(registration_time__lte=ref_date)))
     context = {'days': days, 'users': users}
     return render(request, 'stats/chart.html', context)
