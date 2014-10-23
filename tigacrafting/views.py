9cd13037b29a820d8d172f194bf5681e007c42af
John Palmer <johnrbpalmer@gmail.com>
2014-10-24 01:57:48 +0200

    fixed tigacrafting photos page and added pagination and time

--- tigacrafting/views.py
+++ tigacrafting/views.py
@@ -6,8 +6,11 @@ from tigaserver_app.models import Photo
 import dateutil.parser
 from django.db.models import Count
 import pytz
+import datetime
 from django.db.models import Max
 from django.views.decorators.clickjacking import xframe_options_exempt
+from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def import_tasks():
@@ -28,14 +31,27 @@ def import_tasks():
             existing_photo = Photo.objects.filter(id=int(task['info'][u'\ufeffid']))
             if existing_photo:
                 this_photo = Photo.objects.get(id=task['info'][u'\ufeffid'])
-                task_model.photo = this_photo
-                task_model.save()
+                # check for tasks that already have this photo: There should not be any BUT I accidentially added photos 802-810 in both the first and second crowdcrafting task batches
+                if CrowdcraftingTask.objects.filter(photo=this_photo).count() > 0:
+                    errors.append('Task with Photo ' + str(this_photo.id) + ' already exists. Not importing this task.')
+                else:
+                    task_model.photo = this_photo
+                    task_model.save()
             else:
                 errors.append('Photo with id=' + task['info'][u'\ufeffid'] + ' does not exist.')
         else:
             warnings.append('Task ' + str(existing_task[0].task_id) + ' already exists, not saved.')
-    print '\n'.join(errors)
-    print '\n'.join(warnings)
+    # write errors and warnings to files that we can check
+    if len(errors) > 0 or len(warnings) > 0:
+        barcelona = pytz.timezone('Europe/Paris')
+        ef = open(settings.MEDIA_ROOT + 'crowdcrafting_error_log.html', 'a')
+        if len(errors) > 0:
+            ef.write('<h1>tigacrafting.views.import_tasks errors</h1><p>' + barcelona.localize(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S UTC%z') + '</p><p>' + '</p><p>'.join(errors) + '</p>')
+        if len(warnings) > 0:
+            ef.write('<h1>tigacrafting.views.import_tasks warnings</h1><p>' + barcelona.localize(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S UTC%z') + '</p><p>' + '</p><p>'.join(warnings) + '</p>')
+        ef.close()
+        print '\n'.join(errors)
+        print '\n'.join(warnings)
     return {'errors': errors, 'warnings': warnings}
 
 
@@ -95,8 +111,17 @@ def import_task_responses():
                 this_user.save()
                 response_model.user = this_user
             response_model.save()
-    print '\n'.join(errors)
-    print '\n'.join(warnings)
+    # write errors and warnings to files that we can check
+    barcelona = pytz.timezone('Europe/Paris')
+    if len(errors) > 0 or len(warnings) > 0:
+        ef = open(settings.MEDIA_ROOT + 'crowdcrafting_error_log.html', 'a')
+        if len(errors) > 0:
+            ef.write('<h1>tigacrafting.views.import_task_responses errors</h1><p>' + barcelona.localize(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S UTC%z') + '</p><p>' + '</p><p>'.join(errors) + '</p>')
+        if len(warnings) > 0:
+            ef.write('<h1>tigacrafting.views.import_task_responses warnings</h1><p>' + barcelona.localize(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S UTC%z') + '</p><p>' + '</p><p>'.join(warnings) + '</p>')
+        ef.close()
+        print '\n'.join(errors)
+        print '\n'.join(warnings)
     return {'errors': errors, 'warnings': warnings}
 
 
@@ -113,7 +138,18 @@ def show_validated_photos(request, type='tiger'):
     individual_responses_dic = {'mosquito': 'mosquito_individual_responses_html', 'site': 'site_individual_responses_html', 'tiger': 'tiger_individual_responses_html'}
     title_dic = {'mosquito': 'Mosquito Validation Results', 'site': 'Breeding Site Validation Results', 'tiger': 'Tiger Mosquito Validation Results'}
     question_dic = {'mosquito': 'Do you see a mosquito in this photo?', 'site': 'Do you see a potential tiger mosquito breeding site in this photo?', 'tiger': 'Is this a tiger mosquito?'}
-    context = {'type': type, 'title': title_dic[type], 'question': question_dic[type], 'validated_tasks': sorted(map(lambda x: {'id': x.id, 'report_type': x.photo.report.type, 'report_creation_time': x.photo.report.creation_time.strftime('%d %b %Y, %H:%M %Z'), 'lat': x.photo.report.lat, 'lon':  x.photo.report.lon, 'photo_image': x.photo.medium_image_(), 'validation_score': round(getattr(x, validation_score_dic[type]), 2), 'neg_validation_score': -1*round(getattr(x, validation_score_dic[type]), 2), 'individual_responses_html': getattr(x, individual_responses_dic[type])}, list(validated_tasks_filtered)), key=lambda x: -x['validation_score'])}
+    validated_task_array = sorted(map(lambda x: {'id': x.id, 'report_type': x.photo.report.type, 'report_creation_time': x.photo.report.creation_time.strftime('%d %b %Y, %H:%M %Z'), 'lat': x.photo.report.lat, 'lon':  x.photo.report.lon, 'photo_image': x.photo.medium_image_(), 'validation_score': round(getattr(x, validation_score_dic[type]), 2), 'neg_validation_score': -1*round(getattr(x, validation_score_dic[type]), 2), 'individual_responses_html': getattr(x, individual_responses_dic[type])}, list(validated_tasks_filtered)), key=lambda x: -x['validation_score'])
+    paginator = Paginator(validated_task_array, 25)
+    page = request.GET.get('page')
+    try:
+        these_validated_tasks = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        these_validated_tasks = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        these_validated_tasks = paginator.page(paginator.num_pages)
+    context = {'type': type, 'title': title_dic[type], 'n_tasks': len(validated_tasks_filtered), 'question': question_dic[type], 'validated_tasks': these_validated_tasks}
     return render(request, 'tigacrafting/validated_photos.html', context)
 
 
