218dacf338daf07419346be8d27305d42d7ea572
John Palmer <johnrbpalmer@gmail.com>
2014-12-17 14:36:20 +0100

    set up map urls so they are automatically set for public domain or localhost depending on debug mode

--- tigamap/views.py
+++ tigamap/views.py
@@ -13,6 +13,7 @@ from django.db.models import Max, Count
 import math
 from django.core.context_processors import csrf
 from tigaserver_app.views import get_n_days, get_n_months
+from django.conf import settings
 
 
 def show_grid_05(request):
@@ -245,6 +246,10 @@ def show_validated_photo_map(request):
 
 
 def show_adult_map(request, type='all'):
+    if settings.DEBUG:
+        current_domain = 'localhost:8000'
+    else:
+        current_domain = 'tigaserver.atrapaeltigre.com'
     href_url_name = 'adult_map_type'
     hrefs = {'coverage': reverse('coverage_map'),
                  'adults_all': reverse('adult_map_type', kwargs={'type': 'all'}),
@@ -261,12 +266,16 @@ def show_adult_map(request, type='all'):
         endpoint = type_dic[type]
     except KeyError:
         endpoint = 'all_adults'
-    context = {'redirect_to': redirect_path, 'hrefs': hrefs, 'end_day': get_n_days(), 'endpoint': endpoint}
+    context = {'domain': current_domain, 'redirect_to': redirect_path, 'hrefs': hrefs, 'end_day': get_n_days(), 'endpoint': endpoint}
     context.update(csrf(request))
     return render(request, 'tigamap/validated_report_map.html', context)
 
 
 def show_site_map(request, type='all'):
+    if settings.DEBUG:
+        current_domain = 'localhost:8000'
+    else:
+        current_domain = 'tigaserver.atrapaeltigre.com'
     href_url_name = 'site_map_type'
     hrefs = {'coverage': reverse('coverage_map'),
                  'adults_all': reverse('adult_map_type', kwargs={'type': 'all'}),
@@ -283,12 +292,16 @@ def show_site_map(request, type='all'):
         endpoint = type_dic[type]
     except KeyError:
         endpoint = 'all_sites'
-    context = {'redirect_to': redirect_path,  'hrefs': hrefs, 'end_month': get_n_months(), 'endpoint': endpoint}
+    context = {'domain': current_domain, 'redirect_to': redirect_path,  'hrefs': hrefs, 'end_month': get_n_months(), 'endpoint': endpoint}
     context.update(csrf(request))
     return render(request, 'tigamap/site_map.html', context)
 
 
 def show_new_coverage_map(request):
+    if settings.DEBUG:
+        current_domain = 'localhost:8000'
+    else:
+        current_domain = 'tigaserver.atrapaeltigre.com'
     href_url_name = 'coverage_map'
     hrefs = {'coverage': reverse('coverage_map'),
                  'adults_all': reverse('adult_map_type', kwargs={'type': 'all'}),
@@ -304,6 +317,6 @@ def show_new_coverage_map(request):
         last_id = CoverageArea.objects.order_by('id').last().id
     else:
         last_id = 0
-    context = {'redirect_to': redirect_path,  'hrefs': hrefs, 'last_id': last_id}
+    context = {'domain': current_domain, 'redirect_to': redirect_path,  'hrefs': hrefs, 'last_id': last_id}
     context.update(csrf(request))
     return render(request, 'tigamap/coverage_map_new.html', context)
