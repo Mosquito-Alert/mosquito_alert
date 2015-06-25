67d9b9472e8340f8df276f10463a983c3363f0e7
John Palmer <johnrbpalmer@gmail.com>
2015-06-25 11:18:28 +0200

    bug fixes

--- tigacrafting/views.py
+++ tigacrafting/views.py
@@ -520,7 +520,7 @@ def expert_report_annotation(request, scroll_position='', tasks_per_page='10', l
             args['pages'] = range(1, objects.paginator.num_pages+1)
             current_pending = ExpertReportAnnotation.objects.filter(user=this_user).filter(Q(tiger_certainty_category__isnull=True) | Q(site_certainty_category__isnull=True)).count()
             args['n_pending'] = current_pending
-            n_complete = ExpertReportAnnotation.objects.filter(user=this_user).filter(tiger_certainty_category__isnull=False,site_certainty_category__isnull=False).count()
+            n_complete = ExpertReportAnnotation.objects.filter(user=this_user).filter(validation_complete=True).count()
             args['n_complete'] = n_complete
             args['n_total'] = n_complete + current_pending
             args['year'] = year
