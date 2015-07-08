556036cf6e342d35da592587f12e2eb8cba42e44
John Palmer <johnrbpalmer@gmail.com>
2015-07-08 17:16:43 +0200

    bug

--- tigacrafting/models.py
+++ tigacrafting/models.py
@@ -183,7 +183,8 @@ class ExpertReportAnnotation(models.Model):
         this_report = self.report
         other_annotations = ExpertReportAnnotation.objects.filter(report=this_report).exclude(user=this_user)
         for ano in other_annotations.all():
-            result += '<p>User: ' + ano.user.username + ', Last Edited: ' + str(ano.last_modified) + '</p>'
+            result += '<p>User: ' + ano.user.username + '</p>'
+            result += '<p>Last Edited: ' + ano.last_modified.strftime("%d %b %Y %H:%m") + ' UTC</p>'
             result += '<p>Tiger Certainty: ' + ano.get_tiger_certainty_category_display() if ano.get_tiger_certainty_category_display() else "" + '</p>'
             result += '<p>Tiger Notes: ' + ano.tiger_certainty_notes + '</p>'
             result += '<p>Site Certainty: ' + ano.get_site_certainty_category_display() if ano.get_site_certainty_category_display() else "" + '</p>'
