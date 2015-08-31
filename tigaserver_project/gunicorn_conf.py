336490b928d515631f8e771caef091ca73f4ce1a
John Palmer <johnrbpalmer@gmail.com>
2015-08-31 17:49:00 +0200

    w4 t600

--- tigaserver_project/gunicorn_conf.py
+++ tigaserver_project/gunicorn_conf.py
@@ -1,6 +1,6 @@
-workers = 17
+workers = 4
 bind = '127.0.0.1:49153'
 max_requests = 5000
-timeout = 60
+timeout = 600
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = 'debug'
