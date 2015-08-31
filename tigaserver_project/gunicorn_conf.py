53c56ef0c32cdd0ff988e8e6b4b940bb62470d41
John Palmer <johnrbpalmer@gmail.com>
2015-08-31 17:17:20 +0200

    w4 t600, urls back mostly

--- tigaserver_project/gunicorn_conf.py
+++ tigaserver_project/gunicorn_conf.py
@@ -1,7 +1,6 @@
-workers = 2
+workers = 4
 bind = '127.0.0.1:49153'
 max_requests = 5000
-timeout = 30
-accesslog = '/var/log/gunicorn/tigaserver.access.log'
+timeout = 600
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = 'debug'
