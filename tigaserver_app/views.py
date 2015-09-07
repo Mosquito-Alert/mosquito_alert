ceea5cb2c712738efc215ff16c48bbc1bdd9d18c
John Palmer <johnrbpalmer@gmail.com>
2015-09-07 11:02:19 +0200

    coverage month model updating

--- tigaserver_app/views.py
+++ tigaserver_app/views.py
@@ -604,6 +604,32 @@ def update_coverage_area_month_model(request):
     return HttpResponse(json.dumps(json_response))
 
 
+def update_coverage_area_month_model():
+    json_response = {'updated': False}
+    # turning off for now
+    if True:
+        if CoverageAreaMonth.objects.all().count() > 0:
+            latest_report_server_upload_time = CoverageAreaMonth.objects.order_by('latest_report_server_upload_time').last().latest_report_server_upload_time
+            latest_fix_id = CoverageAreaMonth.objects.order_by('latest_fix_id').last().latest_fix_id
+        else:
+            latest_report_server_upload_time = pytz.utc.localize(datetime(1970, 1, 1))
+            latest_fix_id = 0
+        if CoverageAreaMonth.objects.all().count() == 0 or latest_report_server_upload_time < Report.objects.order_by('server_upload_time').last().server_upload_time or latest_fix_id < Fix.objects.order_by('id').last().id:
+            report_list = get_latest_reports_qs(Report.objects.exclude(hide=True).filter(Q(package_name='Tigatrapp',  creation_time__gte=settings.IOS_START_TIME) | Q(package_name='ceab.movelab.tigatrapp', package_version__gt=3)).filter(server_upload_time__gt=latest_report_server_upload_time))
+            fix_list = Fix.objects.filter(fix_time__gt=settings.START_TIME, id__gt=latest_fix_id)
+            full_lat_list = [(f.masked_lat, f.fix_time.year, f.fix_time.month) for f in fix_list] + [(r.masked_lat, r.creation_time.year, r.creation_time.month) for r in report_list if r.masked_lat is not None]
+            full_lat_list_m0 = [(f.masked_lat, f.fix_time.year) for f in fix_list] + [(r.masked_lat, r.creation_time.year) for r in report_list if r.masked_lat is not None]
+            full_lat_list_y0 = [(f.masked_lat, f.fix_time.month) for f in fix_list] + [(r.masked_lat, r.creation_time.month) for r in report_list if r.masked_lat is not None]
+            unique_lats = set(full_lat_list)
+            unique_lats_m0 = set(full_lat_list_m0)
+            unique_lats_y0 = set(full_lat_list_y0)
+            [lat_function(this_lat, fix_list, latest_fix_id, report_list, latest_report_server_upload_time) for this_lat in unique_lats]
+            [lat_function_m0(this_lat, fix_list, latest_fix_id, report_list, latest_report_server_upload_time) for this_lat in unique_lats_m0]
+            [lat_function_y0(this_lat, fix_list, latest_fix_id, report_list, latest_report_server_upload_time) for this_lat in unique_lats_y0]
+            json_response['updated'] = True
+    return json.dumps(json_response)
+
+
 class CoverageMapViewSet(ReadOnlyModelViewSet):
     queryset = CoverageArea.objects.all()
     serializer_class = CoverageMapSerializer
