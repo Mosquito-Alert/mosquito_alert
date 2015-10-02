bba912098beb9a40c1df50c6e5f8e9511d24c9ee
John Palmer <johnrbpalmer@gmail.com>
2015-10-02 15:14:13 +0200

    thinning views and imports 1

--- tigaserver_app/urls.py
+++ tigaserver_app/urls.py
@@ -1,39 +1,19 @@
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
-from tigaserver_app import views
-
+import tigaserver_app
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'reports', views.ReportViewSet)
+router.register(r'users', tigaserver_app.views.UserViewSet)
+router.register(r'reports', tigaserver_app.views.ReportViewSet)
+router.register(r'photos', tigaserver_app.views.PhotoViewSet)
+router.register(r'fixes', tigaserver_app.views.FixViewSet)
+router.register(r'coverage_month', tigaserver_app.views.CoverageMonthMapViewSet, base_name='coverage_month')
+router.register(r'all_reports', tigaserver_app.views.AllReportsMapViewSet, base_name='all_reports')
 
-# router.register(r'missions', views.MissionViewSet)
-
-router.register(r'photos', views.PhotoViewSet)
-router.register(r'fixes', views.FixViewSet)
-
-#router.register(r'configuration', views.ConfigurationViewSet)
-
-#router.register(r'all_adults', views.AdultMapViewSetAll, base_name='adult')
-#router.register(r'cat2_adults', views.AdultMapViewSetCatGE2, base_name='cat2_adult')
-#router.register(r'cat1_adults', views.AdultMapViewSetCatGE1, base_name='cat1_adult')
-#router.register(r'all_sites', views.SiteMapViewSetAll, base_name='site')
-#router.register(r'embornals', views.SiteMapViewSetEmbornalsFonts, base_name='embornal')
-#router.register(r'basins', views.SiteMapViewSetBasins, base_name='basin')
-#router.register(r'buckets', views.SiteMapViewSetBucketsWells, base_name='bucket')
-#router.register(r'other_sites', views.SiteMapViewSetOther, base_name='other_site')
-#router.register(r'coverage', views.CoverageMapViewSet, base_name='coverage')
-router.register(r'coverage_month', views.CoverageMonthMapViewSet, base_name='coverage_month')
-router.register(r'all_reports', views.AllReportsMapViewSet, base_name='all_reports')
-
-
-
-urlpatterns = patterns('tigaserver_app.views',
-    url(r'^time_info/$', 'get_data_time_info'),
-#    url(r'^update_coverage/$', 'update_coverage_model'),
-#    url(r'^update_coverage_month/$', 'update_coverage_area_month_model'),
-    url(r'^photos/$', 'post_photo'),
-    url(r'^configuration/$', 'get_current_configuration'),
-    url(r'^missions/$', 'get_new_missions'),
+urlpatterns = patterns('',
+    url(r'^time_info/$', 'tigaserver_app.views.get_data_time_info'),
+    url(r'^photos/$', 'tigaserver_app.views.post_photo'),
+    url(r'^configuration/$', 'tigaserver_app.views.get_current_configuration'),
+    url(r'^missions/$', 'tigaserver_app.views.get_new_missions'),
     url(r'^', include(router.urls)),
 )
