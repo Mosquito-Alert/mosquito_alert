c55f3a704423dabeaf516ee4e6ed63fdb94e1947
aescobarr <agustiescobar@gmail.com>
2016-04-18 16:56:48 +0200

    Adjustments to wsgi.py

--- tigaserver_project/wsgi.py
+++ tigaserver_project/wsgi.py
@@ -7,8 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tigaserver_project.settings")
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/opt/python/current/app')
+
+sys.path.append('/opt/python/run/venv/lib/python2.7/site-packages')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tigaserver_project.settings")
+
 application = get_wsgi_application()
