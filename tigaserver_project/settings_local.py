48647d16777d5b2811bdaa83633dc1440b96bcf1
aescobarr <agustiescobar@gmail.com>
2016-04-19 17:21:31 +0200

    Changed settings to allow RDS env vars

--- tigaserver_project/settings_local.py
+++ tigaserver_project/settings_local.py
@@ -9,19 +9,19 @@ SECRET_KEY = "aaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
 ALLOWED_HOSTS = []
 
-#if 'RDS_DB_NAME' in os.environ:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#            'NAME': os.environ['RDS_DB_NAME'],
-#            'USER': os.environ['RDS_USERNAME'],
-#            'PASSWORD': os.environ['RDS_PASSWORD'],
-#            'HOST': os.environ['RDS_HOSTNAME'],
-#            'PORT': os.environ['RDS_PORT'],
-#        }
-#    }
-#else:
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'tigadata',
