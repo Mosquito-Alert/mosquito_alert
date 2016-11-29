c0566f8f44fc12bd3be70cdcf142e4bee47cd3fe
aescobarr <agustiescobar@gmail.com>
2016-11-29 15:05:24 +0100

    Another tweak to settings_local

--- tigaserver_project/settings_local.py
+++ tigaserver_project/settings_local.py
@@ -9,17 +9,17 @@ SECRET_KEY = "aaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
 ALLOWED_HOSTS = []
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+#if 'RDS_DB_NAME' in os.environ:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
+}
 #else:
 #    DATABASES = {
 #        'default': {
