d7c3cebaa2cbe42233ff24dd908af81f4f96ebd2
aescobarr <agustiescobar@gmail.com>
2017-04-04 16:21:45 +0200

    Changed tabs to spaces in script

--- util_scripts/fill_report_geometry_field.py
+++ util_scripts/fill_report_geometry_field.py
@@ -3,12 +3,13 @@
 import os, sys
 
 proj_path = "/home/webuser/webapps/tigaserver/"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","tigaserver_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tigaserver_project.settings")
 sys.path.append(proj_path)
 
 os.chdir(proj_path)
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
 
 from tigaserver_app.models import Report
@@ -18,9 +19,9 @@ reports = Report.objects.all()
 i = len(reports)
 j = 0
 for report in reports:
-	if report.point is None:
-		if report.get_point() is not None:
-			report.point = report.get_point()
-			report.save()
-	j = j + 1
-	print 'Fixed report ' + str(j) + ' of ' + str(i)
+    if report.point is None:
+        if report.get_point() is not None:
+            report.point = report.get_point()
+            report.save()
+    j = j + 1
+    print 'Fixed report ' + str(j) + ' of ' + str(i)
