f2954d7264c425e2dbbe2144babb369c8f452ec7
aescobarr <agustiescobar@gmail.com>
2017-04-27 18:21:50 +0200

    Initial version of gauge graph

--- stats/static/stats/workload.js
+++ stats/static/stats/workload.js
@@ -148,6 +148,67 @@ $(function () {
         $("#userlist_pending").append('<li style="color:' + users[index].color + '" class="list-group-item small-font"><input id="pending_' + users[index].username + '" onclick="javascript:load_pending_report_ajax(\'' + users[index].username + '\')" type="checkbox">' + users[index].name + '</li>');
     });
 
+    gaugeChart = Highcharts.chart('container_gauge',{
+        chart: {
+            type: 'solidgauge'
+        },
+        title: {
+            text: 'Unassigned reports'
+        },
+        pane: {
+            center: ['50%', '85%'],
+            size: '140%',
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }
+        },
+        tooltip: {
+            enabled: false
+        },
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.1, '#55BF3B'], // green
+                [0.5, '#DDDF0D'], // yellow
+                [0.9, '#DF5353'] // red
+            ],
+            lineWidth: 0,
+            minorTickInterval: null,
+            tickAmount: 9,
+            title: {
+                y: -70
+            },
+            labels: {
+                y: 16
+            },
+            min: 0,
+            max: 100
+        },
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        },
+        series: [{
+            name: 'Reports',
+            data: [88],
+            dataLabels: {
+                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                   '<span style="font-size:12px;color:silver"> available reports</span></div>'
+            }
+        }]
+    });
+
     pendingChart = Highcharts.chart('container_pending',{
         chart: {
             type: 'bar'
