8c702d50d60ef3ac2b3bfb835c47f98d8cf87a7f
aescobarr <agustiescobar@gmail.com>
2018-06-05 18:13:00 +0200

    Tweaked gunicorn configuration for test

--- tigaserver_project/gunicorn_conf.py
+++ tigaserver_project/gunicorn_conf.py
@@ -1,6 +1,7 @@
 workers = 8
 bind = '127.0.0.1:49153'
 max_requests = 5000
-timeout = 1200
+#timeout = 1200
+timeout = 180
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = 'debug'
