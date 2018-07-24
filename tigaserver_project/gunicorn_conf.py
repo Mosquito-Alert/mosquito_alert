39d9ddd4947d07f230f1139bd5285a61357a1576
aescobarr <agustiescobar@gmail.com>
2018-07-24 22:40:37 +0200

    Changed timeout

--- tigaserver_project/gunicorn_conf.py
+++ tigaserver_project/gunicorn_conf.py
@@ -2,7 +2,7 @@
 workers = 10
 bind = '127.0.0.1:49153'
 max_requests = 5000
-timeout = 1200
-#timeout = 420
+#timeout = 1200
+timeout = 420
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = 'debug'
