23790a746f34b6a75678f0e9de1daff2f5634411
aescobarr <agustiescobar@gmail.com>
2020-02-18 11:02:25 +0100

    Added trivial test case, test fixtures

--- tigaserver_app/tests.py
+++ tigaserver_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from tigaserver_app.models import Report, EuropeCountry
+from django.core.management import call_command
+
+class ReportTestCase(TestCase):
+    fixtures = ['europe_countries.json','tigausers.json','reports.json']
+
+    def test_fixtures_being_loaded(self):
+        r = EuropeCountry.objects.get(pk=1)
+        self.assertEqual(r.gid,1)
