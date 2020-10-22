ab3bdd190b4e7571c1ff2d2350eac6411fe7516b
aescobarr <agustiescobar@gmail.com>
2020-10-22 08:34:13 +0200

    Testing new encoded notifications, just el locale

--- tigaserver_app/models.py
+++ tigaserver_app/models.py
@@ -2130,7 +2130,7 @@ def package_number_allows_notification(report):
 def issue_notification(report, reason_label, xp_amount, current_domain):
     if getattr( conf, 'DISABLE_ACHIEVEMENT_NOTIFICATIONS', True) == False:
         if package_number_allows_notification(report):
-            table = {k: '&{};'.format(v) for k, v in html.entities.codepoint2name.items()}
+            #table = {k: '&{};'.format(v) for k, v in html.entities.codepoint2name.items()}
             notification_content = NotificationContent()
             context_es = {}
             context_ca = {}
@@ -2159,20 +2159,16 @@ def issue_notification(report, reason_label, xp_amount, current_domain):
             context_en['amount_awarded'] = xp_amount
             context_ca['amount_awarded'] = xp_amount
 
-            context_es['reason_awarded'] = get_translation_in(reason_label, 'es').translate(table)
-            context_en['reason_awarded'] = get_translation_in(reason_label, locale_for_en).translate(table)
-            context_ca['reason_awarded'] = get_translation_in(reason_label, 'ca').translate(table)
+            context_es['reason_awarded'] = get_translation_in(reason_label, 'es').encode('ascii', 'xmlcharrefreplace').decode('UTF-8')
+            context_en['reason_awarded'] = get_translation_in(reason_label, locale_for_en).encode('ascii', 'xmlcharrefreplace').decode('UTF-8')
+            context_ca['reason_awarded'] = get_translation_in(reason_label, 'ca').encode('ascii', 'xmlcharrefreplace').decode('UTF-8')
 
             notification_content.body_html_es = render_to_string('tigaserver_app/award_notification_es.html', context_es)
-            notification_content.body_html_es = notification_content.body_html_es.replace('&amp;','&')
             notification_content.body_html_ca = render_to_string('tigaserver_app/award_notification_ca.html', context_ca)
-            notification_content.body_html_ca = notification_content.body_html_ca.replace('&amp;','&')
             try:
                 notification_content.body_html_en = render_to_string('tigaserver_app/award_notification_' + locale_for_en + '.html', context_en)
-                notification_content.body_html_en = notification_content.body_html_en.replace('&amp;','&')
             except TemplateDoesNotExist:
                 notification_content.body_html_en = render_to_string('tigaserver_app/award_notification_en.html',context_en)
-                notification_content.body_html_en = notification_content.body_html_en.replace('&amp;', '&')
 
             '''
             notification_content.body_html_es = notification_content.body_html_es.decode('utf-8').encode('ascii','xmlcharrefreplace')
