d1c00f25d20721045c5ca83e20d65112ab3f9f95
aescobarr <agustiescobar@gmail.com>
2021-07-02 10:57:55 +0200

    Logging and number of pictures parameter

--- util_scripts/move_heic_pictures.py
+++ util_scripts/move_heic_pictures.py
@@ -1,5 +1,6 @@
 import os, sys
 import whatimage
+import logging
 import pyheif
 import io
 from PIL import Image
@@ -20,13 +21,19 @@ from tigaserver_app.models import Photo
 
 PICTURES_DIR = '/home/webuser/webapps/tigaserver/media/tigapics/'
 BACKUP_DIR = '/home/webuser/webapps/original_pics/'
+NUM_FILES = None
+DEFAULT_NUM_FILES = 50
+logging.basicConfig(filename='heic_converter.log', level=logging.DEBUG)
 
 
 def get_file_list(dirpath):
-    return sorted(Path(dirpath).iterdir(), key=os.path.getmtime, reverse=True)
+    logging.debug("Getting file list")
+    paths = sorted(Path(dirpath).iterdir(), key=os.path.getmtime, reverse=True)
+    return paths[:NUM_FILES]
 
 
 def convert_to_jpg(file):
+    logging.debug("Converting file {0} to jpg".format(str(file)))
     no_ext = os.path.splitext(file)[0]
     heif_file = pyheif.read(file)
     image = Image.frombytes(
@@ -41,10 +48,12 @@ def convert_to_jpg(file):
 
 
 def rename_photo(filename):
+    logging.debug("Renaming photo in database")
     no_ext = os.path.splitext(filename)[0]
     no_route = ntpath.basename(no_ext)
     p = Photo.objects.filter(photo__icontains=no_route)
     if p.exists():
+        logging.debug("Photo found, renaming to {0}".format('tigapics/' + no_route + ".jpg"))
         pic = p.first()
         pic.photo = 'tigapics/' + no_route + ".jpg"
         pic.save()
@@ -56,6 +65,7 @@ def process_file(p):
             data = f.read()
         fmt = whatimage.identify_image(data)
         if fmt == 'heic':
+            logging.debug("Found heic file! {0} files".format(str(p)))
             print("{0} - {1}".format(p, fmt))
             convert_to_jpg(p)
             rename_photo(p)
@@ -68,10 +78,17 @@ def process_files(file_list):
 
 
 def move_file_to_backup(f,backup_dir):
+    logging.debug("Moving file {0} to backup directory {1}".format(str(f),backup_dir))
     no_route = ntpath.basename(f)
     os.rename(str(f), backup_dir + no_route)
 
 
 if __name__ == "__main__":
+    try:
+        NUM_FILES = int(sys.argv[1])
+        logging.debug("Starting scan of {0} files".format(str(NUM_FILES)))
+    except:
+        NUM_FILES = DEFAULT_NUM_FILES
+        logging.debug("Starting scan of {0} files".format(str(NUM_FILES)))
     paths = get_file_list(PICTURES_DIR)
     process_files(paths)
