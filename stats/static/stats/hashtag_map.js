74b6daa9bb626518c9ae567290f85844ce80646a
aescobarr <agustiescobar@gmail.com>
2022-09-30 10:44:51 +0200

    Fixed hashtag map base layer, small typo

--- stats/static/stats/hashtag_map.js
+++ stats/static/stats/hashtag_map.js
@@ -20,13 +20,17 @@ $( document ).ready(function() {
     var mymap = L.map('map').setView([37.53097889440026,1.130859375], 5);
     var markers = L.markerClusterGroup();
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	/*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox.streets'
-	}).addTo(mymap);
+	}).addTo(mymap);*/
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
 
 	var load_data = function(hashtag){
 	    $('#info_window').empty();
