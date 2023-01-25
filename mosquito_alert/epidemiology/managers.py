302ed1bc5861875ea586d06f36272f44ec913ca6
Enric Pou <enricpou@gmail.com>
2023-01-25 13:33:30 +0100

    Fixed MonthlyDistributionManager queryset filtering

--- mosquito_alert/epidemiology/managers.py
+++ mosquito_alert/epidemiology/managers.py
@@ -5,5 +5,5 @@ class MonthlyDistributionManager(Manager):
     def get_queryset(self):
         """Sets the custom queryset as the default."""
         return (
-            super().get_queryset().select_related("taxa__disease_vector")
+            super().get_queryset().exclude(taxon__disease_vector__isnull=True)
         )  # Get only those that are disesases_vectors
