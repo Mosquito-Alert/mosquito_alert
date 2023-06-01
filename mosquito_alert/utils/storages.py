fbb739f5925ed4f43b3662e98d6d713a335e314b
Enric Pou <enricpou@gmail.com>
2023-06-01 10:18:27 +0200

    Added production settings.

--- mosquito_alert/utils/storages.py
+++ mosquito_alert/utils/storages.py
@@ -1,7 +1,7 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage, S3StaticStorage
 
 
-class StaticRootS3Boto3Storage(S3Boto3Storage):
+class StaticRootS3Boto3Storage(S3StaticStorage):
     location = "static"
     default_acl = "public-read"
 
