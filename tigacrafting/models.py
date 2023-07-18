d69cabafe57e7107c7f18969dbd760e68a3d8236
aescobarr <agustiescobar@gmail.com>
2023-07-18 11:27:47 +0200

    Added ia table

--- tigacrafting/models.py
+++ tigacrafting/models.py
@@ -542,6 +542,21 @@ class FavoritedReports(models.Model):
     class Meta:
         unique_together = ('user','report',)
 
+
+class AimaAlertLog(models.Model):
+    xvb = models.BooleanField('Stands for expert validation based - if True, means that the alert was sent after expert validation. For False, it was sent before')
+    report = models.ForeignKey('tigaserver_app.Report', related_name='alerts', help_text='Report related with the alert', on_delete=models.CASCADE)
+    report_datetime = models.DateTimeField()
+    locCode = models.CharField('Locale code - can be either a nuts3 code, or a nuts3_natcode (natcode is a municipality code) for spanish reports',max_length=25)
+    catId = models.IntegerField('aima species category id')
+    species = models.CharField('Species slug',max_length=25)
+    certainty = models.FloatField('IA certainty value')
+    status = models.CharField('Introduction status label',max_length=50, blank=True, null=True)
+    hit = models.BooleanField('True if AIMA identification was initially correct, False if AIMA initially failed and was revised', blank=True, null=True)
+    review_species = models.CharField('Revised species slug, can be empty', max_length=25, blank=True, null=True)
+    review_status = models.CharField('Revised introduction status label', max_length=50, blank=True, null=True)
+    review_datetime =  models.DateTimeField('revision timestamp', blank=True, null=True )
+
 # class Species(models.Model):
 #     species_name = models.TextField('Scientific name of the objective species or combination of species', blank=True, help_text='This is the species latin name i.e Aedes albopictus')
 #     composite = models.BooleanField(default=False, help_text='Indicates if this row is a single species or a combination')
