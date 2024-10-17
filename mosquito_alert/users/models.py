d8bb9dccee2568047f6b291de47c30d4425a07b8
Enric Pou <e.pou@ceab.csic.es>
2024-10-17 13:07:12 +0200

    Use settings.AUTH_USER_MODEL instead of User for FK

--- mosquito_alert/users/models.py
+++ mosquito_alert/users/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import CharField
@@ -31,7 +32,9 @@ class User(AbstractUser):
 
 class UserProfile(BaseIdentifierProfile):
     # Relations
-    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, related_name="profile")
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, primary_key=True, on_delete=models.CASCADE, related_name="profile"
+    )
 
     # Attributes - Mandatory
     # Attributes - Optional
