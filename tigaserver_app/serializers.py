2ceada0396e7262a065a6075c3f1483e5b47d98e
Enric Pou <enricpou@gmail.com>
2025-01-07 10:45:25 +0100

    Fix MapDataSerializer fields

--- tigaserver_app/serializers.py
+++ tigaserver_app/serializers.py
@@ -562,7 +562,38 @@ class MapDataSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Report
-        exclude = ('version_number', 'user', 'report_id', 'server_upload_time', 'phone_upload_time', 'version_time', 'location_choice', 'current_location_lon', 'current_location_lat', 'mission', 'selected_location_lon', 'selected_location_lat', 'note', 'package_name', 'package_version', 'device_manufacturer', 'device_model', 'os', 'os_version', 'os_language', 'app_language', 'hide', 'timezone')
+        fields = (
+            'version_UUID',
+            'creation_time',
+            'creation_date',
+            'creation_day_since_launch',
+            'creation_year',
+            'creation_month',
+            'site_cat',
+            'type',
+            'lon',
+            'lat',
+            'movelab_annotation',
+            'movelab_annotation_euro',
+            'tiger_responses',
+            'tiger_responses_text',
+            'site_responses',
+            'site_responses_text',
+            'tigaprob_cat',
+            'latest_version',
+            'visible',
+            'n_photos',
+            'final_expert_status_text',
+            'responses',
+            'country',
+            'updated_at',
+            'datetime_fix_offset',
+            'point',
+            'nuts_2',
+            'nuts_3',
+            'cached_visible',
+            'session',
+        )
 
 
 class SiteMapSerializer(serializers.ModelSerializer):
