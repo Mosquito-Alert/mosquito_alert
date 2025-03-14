a32eb1e1bc6a274cb29810b5c20e725b282980b7
Enric Pou <enricpou@gmail.com>
2025-03-14 08:56:46 +0100

    Make it multiprocessing

--- tigacrafting/management/commands/refresh_identification_tasks.py
+++ tigacrafting/management/commands/refresh_identification_tasks.py
@@ -1,7 +1,18 @@
 from django.core.management.base import BaseCommand
+from multiprocessing import Pool
+import os
 from tqdm import tqdm
 
 from tigacrafting.models import IdentificationTask
+
+def refresh_task(task_data):
+    from django import db
+    db.connections.close_all()
+    task, force = task_data
+    try:
+        task.refresh(force=force, commit=True)
+    except Exception as e:
+        print(task.pk, e)
 
 class Command(BaseCommand):
     help = 'Refresh identification tasks'
@@ -17,8 +28,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         identification_tasks_qs = IdentificationTask.objects.all().select_related('report', 'taxon')
 
-        for task in tqdm(identification_tasks_qs.iterator(), total=identification_tasks_qs.count()):
-            try:
-                task.refresh(force=options['force'], commit=True)
-            except Exception as e:
-                print(task.pk, e)
+        # Prepare task data (task and force option) to pass to worker pool
+        tasks_data = ((task, options['force']) for task in identification_tasks_qs.iterator())
+
+        # Calculate 80% of the total processors, but ensure it's at least 1
+        num_processors = max(1, int(os.cpu_count() * 0.8))
+
+        # Create a Pool with the calculated number of processes
+        with Pool(processes=num_processors) as pool:
+            # Use tqdm to show progress bar with pool.imap
+            list(tqdm(pool.imap_unordered(refresh_task, tasks_data, chunksize=100), total=identification_tasks_qs.count()))
