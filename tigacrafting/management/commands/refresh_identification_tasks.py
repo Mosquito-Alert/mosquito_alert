4c20c3027f23dc913a2f377e8579bc43d6a89497
Enric Pou <enricpou@gmail.com>
2025-03-18 14:56:21 +0100

    Added --multiprocessing

--- tigacrafting/management/commands/refresh_identification_tasks.py
+++ tigacrafting/management/commands/refresh_identification_tasks.py
@@ -25,16 +25,30 @@ class Command(BaseCommand):
             help='Force refresh (use force=True)',
         )
 
+        parser.add_argument(
+            '--multiprocessing',
+            action='store_true',
+            help='Use multiprocessing for refreshing tasks',
+        )
+
     def handle(self, *args, **options):
         identification_tasks_qs = IdentificationTask.objects.all().select_related('report', 'taxon')
 
-        # Prepare task data (task and force option) to pass to worker pool
-        tasks_data = ((task, options['force']) for task in identification_tasks_qs.iterator())
+        if options['multiprocessing']:
+            # Prepare task data (task and force option) to pass to worker pool
+            tasks_data = ((task, options['force']) for task in identification_tasks_qs.iterator())
 
-        # Calculate 80% of the total processors, but ensure it's at least 1
-        num_processors = max(1, int(os.cpu_count() * 0.8))
+            # Calculate 80% of the total processors, but ensure it's at least 1
+            num_processors = max(1, int(os.cpu_count() * 0.8))
 
-        # Create a Pool with the calculated number of processes
-        with Pool(processes=num_processors) as pool:
-            # Use tqdm to show progress bar with pool.imap
-            list(tqdm(pool.imap_unordered(refresh_task, tasks_data, chunksize=100), total=identification_tasks_qs.count()))
+            # Create a Pool with the calculated number of processes
+            with Pool(processes=num_processors) as pool:
+                # Use tqdm to show progress bar with pool.imap
+                list(tqdm(pool.imap_unordered(refresh_task, tasks_data, chunksize=100), total=identification_tasks_qs.count()))
+        else:
+            # If multiprocessing is not enabled, handle tasks without it
+            for task in tqdm(identification_tasks_qs.iterator(), total=identification_tasks_qs.count()):
+                try:
+                    task.refresh(force=options['force'], commit=True)
+                except Exception as e:
+                    print(task.pk, e)
