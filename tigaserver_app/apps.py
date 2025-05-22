967e94e61a2279009252783339b6706421a12c1f
Enric Pou <enricpou@gmail.com>
2025-05-22 13:41:35 +0200

    Bugfix: jpeg are not longer open using rawpy

--- tigaserver_app/apps.py
+++ tigaserver_app/apps.py
@@ -2,12 +2,23 @@ from django.apps import AppConfig
 
 from timezonefinder import TimezoneFinder
 
+from PIL import Image, TiffImagePlugin
 from .pillow_utils import register_raw_opener
 from pillow_heif import register_heif_opener
 
+# NOTE: calling Image.init() before register is crucial to import first
+# native plugins from pillow before the custom one.
+# Besides, there is not accept() function defined for raw_opener, which
+# does not check for any magic number (bytes prefix) for accept only certain
+# types of images.
+Image.init()
 register_heif_opener()
 register_raw_opener()
 
+if TiffImagePlugin.TiffImageFile.format in Image.ID:
+    #NOTE: Tiff is accepting magic numbers from RAW photos. Should be the last in the chain.
+    Image.ID.remove(TiffImagePlugin.TiffImageFile.format)
+    Image.ID.append(TiffImagePlugin.TiffImageFile.format)
 
 class TigaserverApp(AppConfig):
     name = "tigaserver_app"
