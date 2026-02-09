94cbe4d1dcdb92e3271f826b9c3faa1c87968181
Enric Pou <enricpou@gmail.com>
2026-02-09 15:15:51 +0100

    Use Report textchoid for breeding sites

--- api/tests/integration/breeding_sites/factories.py
+++ api/tests/integration/breeding_sites/factories.py
@@ -6,6 +6,6 @@ from api.tests.factories import create_report_object
 def create_breeding_site_object(user: TigaUser) -> Report:
     report = create_report_object(user)
     report.type = Report.TYPE_SITE
-    report.breeding_site_type = Report.BREEDING_SITE_TYPE_STORM_DRAIN
+    report.breeding_site_type = Report.BreedingSiteType.STORM_DRAIN
     report.save()
     return report
