ce9ae38d4fc9bf9b1dbf4a798c9cf17df0954459
Enric Pou <enricpou@gmail.com>
2026-02-17 16:58:16 +0100

    Allow sentry-trace CORS

--- tigaserver_project/settings.py
+++ tigaserver_project/settings.py
@@ -4,6 +4,7 @@ import django.conf.global_settings as DEFAULT_SETTINGS
 from datetime import datetime
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.gdal import DataSource
+from corsheaders.defaults import default_headers
 
 from zoneinfo import ZoneInfo
 
@@ -119,6 +120,13 @@ CORS_ALLOWED_ORIGINS = [
     "https://map.mosquitoalert.com",
     "https://map2.mosquitoalert.com"
 ]
+
+
+CORS_ALLOW_HEADERS = (
+    *default_headers,
+    'sentry-trace',
+    'baggage',
+)
 
 ROOT_URLCONF = 'tigaserver_project.urls'
 
