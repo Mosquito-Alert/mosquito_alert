e41025865226d73a1508359fe3974dff412e15b2
Enric Pou <enricpou@gmail.com>
2026-03-25 13:08:58 +0100

    Remove CORS only for /api. Not working when host is api

--- config/settings/base.py
+++ config/settings/base.py
@@ -397,7 +397,6 @@ SIMPLE_JWT = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 # -------------------------------------------------------------------------------
-CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
 # For django-hosts
